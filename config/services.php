5fb5fffdfa5b0c8cc1c13b308282b09d68276c0d
Joan Font <joanfont@gmail.com>
2025-11-27 17:13:32 +0100

    More fixes

--- config/services.php
+++ config/services.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Symfony\Component\DependencyInjection\Loader\Configurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
-use function env;
-use function param;
-use function service;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\env;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\param;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
+
 
 return function (ContainerConfigurator $container): void {
     $parameters = $container->parameters();
@@ -27,19 +28,19 @@ return function (ContainerConfigurator $container): void {
         ->private();
 
     $services
-        ->instanceof(\App\Application\Commons\Command\CommandHandler::class)
+        ->instanceof(App\Application\Commons\Command\CommandHandler::class)
         ->tag('messenger.message_handler', ['bus' => 'command.bus']);
 
     $services
-        ->instanceof(\App\Application\Commons\Event\EventHandler::class)
+        ->instanceof(App\Application\Commons\Event\EventHandler::class)
         ->tag('messenger.message_handler', ['bus' => 'event.bus']);
 
     $services
-        ->instanceof(\App\Application\Commons\Query\QueryHandler::class)
+        ->instanceof(App\Application\Commons\Query\QueryHandler::class)
         ->tag('messenger.message_handler', ['bus' => 'query.bus']);
 
     $services
-        ->instanceof(\App\Infrastructure\Doctrine\Type\Type::class)
+        ->instanceof(App\Infrastructure\Doctrine\Type\Type::class)
         ->tag('app.doctrine.type');
 
     $services
@@ -57,46 +58,46 @@ return function (ContainerConfigurator $container): void {
         ->tag('controller.argument_value_resolver');
 
     $services
-        ->set(\App\Infrastructure\Doctrine\Listener\EntityEventsListener::class)
+        ->set(App\Infrastructure\Doctrine\Listener\EntityEventsListener::class)
         ->tag('doctrine.event_listener', ['event' => 'postPersist'])
         ->tag('doctrine.event_listener', ['event' => 'postUpdate'])
         ->tag('doctrine.event_listener', ['event' => 'postRemove']);
 
     $services
         ->alias(
-            \App\Application\Commons\Command\CommandBus::class,
-            \App\Infrastructure\Symfony\Messenger\MessengerCommandBus::class,
+            App\Application\Commons\Command\CommandBus::class,
+            App\Infrastructure\Symfony\Messenger\MessengerCommandBus::class,
         )->public();
 
     $services
         ->alias(
-            \App\Application\Commons\Query\QueryBus::class,
-            \App\Infrastructure\Symfony\Messenger\MessengerQueryBus::class,
+            App\Application\Commons\Query\QueryBus::class,
+            App\Infrastructure\Symfony\Messenger\MessengerQueryBus::class,
         )->public();
 
     $services
         ->alias(
-            \App\Application\Commons\Event\EventBus::class,
-            \App\Infrastructure\Symfony\Messenger\MessengerEventBus::class,
+            App\Application\Commons\Event\EventBus::class,
+            App\Infrastructure\Symfony\Messenger\MessengerEventBus::class,
         )->public();
 
     $services
-        ->set(\App\Application\Command\Registration\RegisterParticipantHandler::class)
+        ->set(App\Application\Command\Registration\RegisterParticipantHandler::class)
         ->arg('$maxSegmentsPerParticipant', param('app.registration.max_registrations_per_participant'));
 
     $services
-        ->set('app.filesystem.project', \App\Application\Service\File\Filesystem::class)
-        ->factory([\App\Infrastructure\League\Service\File\LeagueFilesystemFactory::class, 'makeLocal'])
+        ->set('app.filesystem.project', App\Application\Service\File\Filesystem::class)
+        ->factory([App\Infrastructure\League\Service\File\LeagueFilesystemFactory::class, 'makeLocal'])
         ->arg('$root', param('kernel.project_dir'));
 
     $services
-        ->set('app.filesystem.uploads', \App\Application\Service\File\Filesystem::class)
-        ->factory([\App\Infrastructure\League\Service\File\LeagueFilesystemFactory::class, 'makeLocal'])
+        ->set('app.filesystem.uploads', App\Application\Service\File\Filesystem::class)
+        ->factory([App\Infrastructure\League\Service\File\LeagueFilesystemFactory::class, 'makeLocal'])
         ->arg('$root', param('app.uploads.local.dir'));
 
     $services
-        ->set('app.filesystem.root', \App\Application\Service\File\Filesystem::class)
-        ->factory([\App\Infrastructure\League\Service\File\LeagueFilesystemFactory::class, 'makeLocal'])
+        ->set('app.filesystem.root', App\Application\Service\File\Filesystem::class)
+        ->factory([App\Infrastructure\League\Service\File\LeagueFilesystemFactory::class, 'makeLocal'])
         ->arg('$root', '/');
 
     $services
@@ -109,33 +110,33 @@ return function (ContainerConfigurator $container): void {
         ->alias('app.registration.registration_created.send_email.filesystem', 'app.filesystem.project');
 
     $services
-        ->set(\App\Application\Command\Route\ImportRoutesFromFileHandler::class)
+        ->set(App\Application\Command\Route\ImportRoutesFromFileHandler::class)
         ->arg('$filesystem', service('app.route.import_routes_from_file.filesystem'));
 
     $services
-        ->set(\App\Application\Command\Route\ImportItinerariesFromFileHandler::class)
+        ->set(App\Application\Command\Route\ImportItinerariesFromFileHandler::class)
         ->arg('$filesystem', service('app.route.import_segments_from_file.filesystem'));
 
     $services
-        ->set(\App\Application\Command\Route\ImportSegmentsFromFileHandler::class)
+        ->set(App\Application\Command\Route\ImportSegmentsFromFileHandler::class)
         ->arg('$filesystem', service('app.route.import_segments_from_file.filesystem'));
 
     $services
-        ->set(\App\Infrastructure\Symfony\Mailer\Notification\EmailRegistrationCreatedNotification::class)
+        ->set(App\Infrastructure\Symfony\Mailer\Notification\EmailRegistrationCreatedNotification::class)
         ->arg('$from', param('app.email.from'))
         ->arg('$filesystem', service('app.registration.registration_created.send_email.filesystem'))
         ->arg('$templatePath', '/templates/registrationCreated.html.twig');
 
     $services
-        ->set(\App\Infrastructure\Symfony\Http\File\SymfonyFileUploader::class)
+        ->set(App\Infrastructure\Symfony\Http\File\SymfonyFileUploader::class)
         ->arg('$rootFilesystem', service('app.filesystem.root'))
         ->arg('$uploadsFilesystem', service('app.filesystem.uploads'));
 
     $services
-        ->set(\App\Infrastructure\Symfony\Http\File\SymfonyLocalUrlGenerator::class)
+        ->set(App\Infrastructure\Symfony\Http\File\SymfonyLocalUrlGenerator::class)
         ->arg('$prefix', param('app.uploads.local.prefix'));
 
     $services
-        ->set(\App\Infrastructure\Symfony\Validator\Constraints\MaxSegmentsCountValidator::class)
+        ->set(App\Infrastructure\Symfony\Validator\Constraints\MaxSegmentsCountValidator::class)
         ->arg('$maxSegmentsPerParticipant', param('app.registration.max_registrations_per_participant'));
 };
